e37d527f2cdbe126a52e4add784f1d870ea32fc8
nforne <wnforne@gmail.com>
2021-10-26 01:49:13 +0000

    added tail function - array without array[0]

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tail = "";
+  if (array.length === 0) {
+    tail = [];
+  } else if (array.length !== 0) {
+    tail = array.slice(1);
+  }
+  return tail;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
