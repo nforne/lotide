b3568ca94882ba95557d158fd9a48ca2d64ec5ec
nforne <wnforne@gmail.com>
2021-10-26 23:35:20 +0000

    add flatten.js - array flattening function

---
+++ flatten.js
@@ -0,0 +1,16 @@
+const flatten = function(array) {
+  let outPut = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        outPut.push(array[i][j]);
+      }
+    } else {
+      outPut.push(array[i]);
+    }
+  }
+  return outPut;
+}
+
+let example = flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+console.log(example)
