4ea9aa6f16dee6318c353f29f2ed139291fcde2e
nforne <wnforne@gmail.com>
2021-10-26 19:19:01 +0000

    add head

--- head.js
+++ head.js
@@ -1,3 +1,5 @@
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
@@ -5,6 +7,8 @@ const assertEqual = function(actual, expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   }
 };
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 const head = function(array) {
   let head = "";
@@ -18,3 +22,5 @@ const head = function(array) {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
