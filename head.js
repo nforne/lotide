a3ba2603be7afdd02d76c410016b0b20b24279c8
nforne <wnforne@gmail.com>
2021-10-26 01:16:20 +0000

    added the head function return array[0] or 'undefined'

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const head = function (array) {
+  let head = "";
+  if (array.length === 0) {
+    head = "undefined";
+  } else if (array.length !== 0) {
+    head = array[0];
+  }
+  return head;
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
