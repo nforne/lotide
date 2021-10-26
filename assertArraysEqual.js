0fb4111c5e621286837d2335121eba6a9f6541fd
nforne <wnforne@gmail.com>
2021-10-26 19:23:06 +0000

    add assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = function(array1, array2) {
+  let outPut = "";
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        outPut = `🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`;
+        break;
+      } else {
+        outPut = `✅✅✅Assertion Passed: ${array1} === ${array2}`;
+      }
+    }
+  } else {
+    outPut = `🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`;
+  }
+  console.log(outPut);
+};
