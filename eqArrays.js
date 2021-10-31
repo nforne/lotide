1332e8d72c99fcaf46974cf745beeefb909b1c90
nforne <mnforne@gmail.com>
2021-10-31 16:19:39 -0700

    eslint eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -10,18 +10,18 @@ const eqArrays = function(array1, array2) {
   let outPut = true;
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {        
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
         if (!eqArrays(array1[i], array2[i])) {
           outPut = false;
           break;
-        } 
+        }
       } else if (array1[i] !== array2[i]) {
         outPut = false;
         break;
-      } 
-    } 
+      }
+    }
   } else {
-    outPut = false; 
+    outPut = false;
   }
   return outPut;
 };
