078070e2a1df4edc0ce35626f61ec19484ca5c71
nforne <mnforne@gmail.com>
2021-11-01 13:59:58 -0700

    refactor with module exports and require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,13 @@
+const eqArrays = require("./eqArrays")
 //function code
 const assertArraysEquals = function(array1, array2) {
   let outPut = "";
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        outPut = `🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`;
-        break;
-      } else {
-        outPut = `✅✅✅Assertion Passed: ${array1} === ${array2}`;
-      }
-    }
+  if (eqArrays(array1, array2)) {
+    outPut = `✅✅✅Assertion Passed: ${array1} === ${array2}`;
   } else {
     outPut = `🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`;
-  }
+    }
   console.log(outPut);
 };
 
-//test code
-assertArraysEquals([1, 2, 3], [1, 2, 5]);
+module.exports = assertArraysEquals;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const assertArraysEquals = require("../assertArraysEqual")
+
+//test code
+assertArraysEquals([1, 2, 3], [1, 2, 5]);
